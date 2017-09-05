2a4941d99cba81a052dad5c50149c89a9d194417
dayvod <dyearwoodd@gmail.com>
2017-09-05 14:59:16 +0000

    Created many to many methods for Stock and User model

--- app/User.php
+++ app/User.php
@@ -23,4 +23,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public function stocks() 
+    {
+        return $this->belongsToMany('App\Stock')->withTimestamps();
+    }
 }
