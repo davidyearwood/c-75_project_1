0732f13c6887ae3cb0090d9f6384067e1d4a6a86
dayvod <dyearwoodd@gmail.com>
2018-01-15 20:46:19 +0000

    Style Portfolio's page table

--- resources/views/layouts/material.blade.php
+++ resources/views/layouts/material.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>CS75 Finance | @yield('title')</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,400,700|Titillium+Web:300,400,700" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
         <link rel="stylesheet" href="/css/normalize.css">
         <link rel="stylesheet" href="/css/stylesheet.css">
--- resources/views/partials/portfolio-table.blade.php
+++ resources/views/partials/portfolio-table.blade.php
@@ -2,23 +2,22 @@
     <caption>My Portfolio</caption>
     <thead>
         <tr>
-            <th>Name</th>
-            <th>Symbol</th>
-            <th>Purchased Date</th>
-            <th>Purchased Price</th>
-            <th>Last Trade Cost</th>
-            <th>Shares</th>
-            <th></th>
+            <!--<th>Name</th>-->
+            <th scope="col">Symbol</th>
+            <th scope="col">Purchased Date</th>
+            <th scope="col">Purchased Price</th>
+            <th scope="col">Last Trade Cost</th>
+            <th scope="col">Shares</th>
+            <th scope="col"></th>
         </tr>
     </thead>
     <tbody>
         @foreach($stocks as $stock)
         <tr>
-            <td>{{ $stock->name }}</td>
             <td>{{ $stock->symbol }}</td>
             <td>{{ $stock->pivot->created_at }}</td>
-            <td>{{ $stock->pivot->purchased_price }}</td>
-            <td>{{ $stock->last_trade_price }}</td>
+            <td class="material__td--numeric">${{ $stock->pivot->purchased_price }}</td>
+            <td class="material__td--numeric">${{ $stock->last_trade_price }}</td>
             <td>{{ $stock->pivot->quantity }}</td>
             <td><a href="/portfolio/{{ $stock->pivot->id }}" class="btn">Sell</a></td>
         </tr>
